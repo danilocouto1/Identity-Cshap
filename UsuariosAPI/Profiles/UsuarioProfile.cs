c5d9bbc216c79b94a453959f3ea52efb43d1f3e8
Danilo <dansantosalfa@gmail.com>
2022-06-27 09:23:26 -0300

    AutoMapper de DtoUsuario para usuario

---
+++ UsuariosAPI/Profiles/UsuarioProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using UsuariosAPI.Data.DTO;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Profiles
+{
+    public class UsuarioProfile : Profile
+    {
+        public UsuarioProfile()
+        {
+            CreateMap<CreateUsuarioDto, Usuario>();
+        }
+    }
+}
