fd27b2577b95d839e84f7f96288d8391e7c5a948
Danilo <dansantosalfa@gmail.com>
2022-06-27 09:19:05 -0300

    Criando novo Controller para cadastrar usuarios (ainda sem metodos)

---
+++ UsuariosAPI/Controllers/CadastroController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosAPI.Data.DTO;
+
+namespace UsuariosAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastrarUsuario(CreateUsuarioDto usuarioDto)
+        {
+            return Ok(usuarioDto);
+        }
+    }
+}
